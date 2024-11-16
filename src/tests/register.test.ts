a71efdc8c8b853c9754715ed821cde56c379f167
oracle-wizard <allenrjean@gmail.com>
2024-11-16 10:36:51 -0500

    change circle ci url to 127

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 test('Registration page - valid registration', async () => {
-  const baseUrl = 'http://localhost:3000';
+  const baseUrl = 'http://127.0.0.1:3000';
 
   const userData = {
     firstName: "new",

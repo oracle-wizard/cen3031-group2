84d2c990386fc9efa672c7fa849f3fb81a919b14
oracle-wizard <allenrjean@gmail.com>
2024-11-24 11:57:30 -0500

    updated login page and navbar

--- src/tests/test.sql
+++ src/tests/test.sql
@@ -1,8 +1,15 @@
 SELECT * FROM "C.SMELTZER".USERS;
+SELECT * FROM "C.SMELTZER".Expense;
+SELECT * FROM "C.SMELTZER".BudgetCategory;
 
-DELETE FROM "C.SMELTZER".users WHERE email = 'newuser1@example.com';
+DELETE FROM "C.SMELTZER".Expense WHERE email = 'newuser@example.com';
 COMMIT;
 
 SELECT * 
 FROM all_tab_privs
 WHERE table_name = 'USERS';
+
+SELECT * FROM "C.SMELTZER".BudgetCategory;
+SELECT * FROM "C.SMELTZER".Expense where email = 'newuser@example.com';
+
+Select * from "C.SMELTZER".BudgetCategory;

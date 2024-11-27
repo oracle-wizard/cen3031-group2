eef456a62c164d339a44ccbc7b39dfe3a4e6f72e
xXLittleBearXx <ethan2@bellsouth.net>
2024-11-27 17:02:51 -0500

    most of the account update

---
+++ server/src/controllers/updateAccountController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { execute } from '../database';
+
+export const updateAccount = async (req: Request, res: Response) => {
+  try {
+      const { firstName, lastName, oldEmail, newEmail } = req.body;
+
+      const query = `
+          UPDATE "C.SMELTZER"."USERS"
+          SET 
+              "FIRST_NAME" = :FIRST_NAME,
+              "LAST_NAME" = :LAST_NAME,
+              "EMAIL" = :NEW_EMAIL,
+          WHERE "EMAIL" = :OLD_EMAIL
+      `;
+
+      const binds = {
+          FIRST_NAME: firstName,
+          LAST_NAME: lastName,
+          OLD_EMAIL: oldEmail,
+          NEW_EMAIL: newEmail
+      };
+
+      await execute(query, binds);
+
+      res.status(200).json({ message: 'Account updated successfully' });
+  } catch (error) {
+      console.error('Error updating account:', error);
+      res.status(500).json({ error: 'Failed to update account' });
+  }
+};
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,7 +4,7 @@ import { refreshToken } from '../controllers/refreshTokenController';
 import {addBudget} from '../controllers/dashboard'
 import authToken from '../middleware/authenticateToken';
 import { getExpenses, addExpense, updateExpense, deleteExpense } from '../controllers/expenseController';
-
+import { updateAccount } from '../controllers/updateAccountController';
 
 const router = Router();
 router.post('/register', register);
@@ -13,12 +13,9 @@ router.post('/refresh-token',  refreshToken)
 router.post('/dashboard', authToken,  addBudget ) ;
 router.get('/dashboard', authToken ) ;
 router.get('/expense-tracker', getExpenses);
-<<<<<<< Updated upstream
 router.post('/expenses', authToken, addExpense);
 router.put('/expenses', authToken, updateExpense);
 router.delete('/expenses', authToken, deleteExpense);
-=======
->>>>>>> Stashed changes
 router.post('/reset-password', resetPassword);
 router.post('/logout', logout);
 router.post('/verify-code', verifyCode);
@@ -26,4 +23,5 @@ router.delete('/delete', deleteUser);
 router.post('/new-password', setNewPassword)
 //router.get('/expenses', getExpenses);
 //router.post('/expenses', addExpense);
+router.put('/account-update', authToken, updateAccount);
 export default router;
